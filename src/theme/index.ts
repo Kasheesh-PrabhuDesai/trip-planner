64301889dc621d4cc3e945633ea7ec342245c415
Kasheesh PrabhuDesai <kasheesh171995@gmail.com>
2022-11-07 17:20:24 +0100

    feat: CREATED reusable material ui custom theme

---
+++ src/theme/index.ts
@@ -0,0 +1,7 @@
+export * from "./theme";
+export * from "./box-shadow";
+export * from "./colors";
+export * from "./hex-to-rgb";
+
+import theme from "./theme";
+export const easyTripPlannerTheme = theme;
